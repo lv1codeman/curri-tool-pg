04d067551e80d367708ff539c6858293a99ef0e0
lv1codeman <lv1codeman@gmail.com>
2026-08-05 16:45:43 +0800

    發布測試

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
     compatibilityVersion: 4,
   },
 
+  // 發布到github page時需改為false，本地測試需改為true
   ssr: false,
 
   // when enabling ssr option you need to disable inlineStyles and maybe devLogs
@@ -35,7 +36,10 @@ export default defineNuxtConfig({
   // 運行時配置 (Runtime Config)
   runtimeConfig: {
     public: {
-      apiBaseUrl: "",
+      // apiBaseUrl: "",
+      apiBaseUrl:
+        process.env.NUXT_PUBLIC_API_BASE_URL ||
+        "https://curridata-server-pg.vercel.app",
     },
   },
   vuetify: {
